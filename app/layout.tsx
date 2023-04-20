398b56fbf43dc5fd2b74720a564d66175242a277
Jordan Roberts <jrobertswebdev@gmail.com>
2023-04-20 00:16:14 -0500

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,11 @@
-import "@/styles/globals.css"
-import { Metadata } from "next"
+import Head from "next/head"
 
-import { siteConfig } from "@/config/site"
+import "@/styles/globals.css"
 import { fontSans } from "@/lib/fonts"
 import { cn } from "@/lib/utils"
 import { SiteHeader } from "@/components/site-header"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { ThemeProvider } from "@/components/theme-provider"
-
-export const metadata: Metadata = {
-  title: {
-    default: siteConfig.name,
-    template: `%s - ${siteConfig.name}`,
-  },
-  description: siteConfig.description,
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
-  ],
-  icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon-16x16.png",
-    apple: "/apple-touch-icon.png",
-  },
-}
 
 interface RootLayoutProps {
   children: React.ReactNode
@@ -33,7 +15,9 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <>
       <html lang="en" suppressHydrationWarning>
-        <head />
+        <Head>
+          <title>Remindas</title>
+        </Head>
         <body
           className={cn(
             "min-h-screen bg-background font-sans antialiased",
--- config/site.ts
+++ config/site.ts
@@ -1,9 +1,8 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Next.js",
-  description:
-    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+  name: "Reminda",
+  description: "A simple reminder app for forgetful people.",
   mainNav: [
     {
       title: "Home",
@@ -11,8 +10,7 @@ export const siteConfig = {
     },
   ],
   links: {
-    twitter: "https://twitter.com/shadcn",
-    github: "https://github.com/shadcn/ui",
-    docs: "https://ui.shadcn.com",
+    twitter: "https://twitter.com/jRobertsWebDev",
+    github: "https://github.com/FlapShatner/remindas",
   },
 }
